1695f6ad90e07f2a8b7016d485284cc0943ce0b8
Shaw Ken Chang <kenchang408@gmail.com>
2013-05-07 00:02:00 -0700

    tweet_sentiment code completed
    Note that we implicitly encoded the files from output.txt into uint-8.
    It's because Powershell has issues reading none uint-8 format .txt.
    It's possible you don't need to do this extra step if you are not using
    windows.

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,17 +1,60 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+sentimentData = sys.argv[1] #AFIN-111.txt
+twitterData = sys.argv[2] #output.txt
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def tweet_dict(twitterData):  
+    ''' (file) -> list of dictionaries
+    This method should take your output.txt
+    file and create a list of dictionaries.
+    '''
+    twitter_list_dict = []
+    twitterfile = open(twitterData)
+    for line in twitterfile:
+        twitter_list_dict.append(json.loads(line.decode('utf-8-sig')))
 
+    return twitter_list_dict
+    #return data_read["text"]
+    
+def sentiment_dict(sentimentData):
+    ''' (file) -> dictionary
+    This method should take your sentiment file
+    and create a dictionary in the form {word: value}
+    '''
+    afinnfile = open(sentimentData)
+    scores = {} # initialize an empty dictionary
+    for line in afinnfile:
+        term, score  = line.split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        scores[term] = float(score)  # Convert the score to an integer.
+
+    return scores # Print every (term, score) pair in the dictionary
+    
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+    tweets = tweet_dict(twitterData)
+    sentiment = sentiment_dict(sentimentData)
+    
+    '''Create a method below that loops through each tweet in your 
+    twees_list.  For each individual tweet it should add up you sentiment 
+    score, based on the sent_dict.
+    '''
+    for index in range(len(tweets)):
+        tweet_word = tweets[index]["text"].split()
+        sent_score = 0
+        for word in tweet_word:
+            
+            if str(word) in sentiment.keys():
+                sent_score = sent_score + float(sentiment[word])
+            else:
+                sent_score = sent_score 
 
+        print float(sent_score)
+    
+
+
+
+
+
+    
 if __name__ == '__main__':
     main()
